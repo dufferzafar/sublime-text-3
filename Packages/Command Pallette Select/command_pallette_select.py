d484e70ee8f055b99e22baf63c53108a0a5cba02
Shadab Zafar <dufferzafar0@gmail.com>
2018-05-18 22:44:50 +0530

    Remove command pallette select since it has been fixed

--- Packages/Command Pallette Select/command_pallette_select.py
+++
@@ -1,9 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class SelectCommandPaletteTextCommand(sublime_plugin.WindowCommand):
-
-    def run(self):
-        self.window.run_command("show_overlay", {"overlay": "command_palette"})
-        self.window.run_command("select_all")
