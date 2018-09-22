2c6e684175e5ead6e71ea3c4bffb963427135248
Shadab Zafar <dufferzafar0@gmail.com>
2018-09-22 12:05:04 +0530

    Jrnl Plugin: Provide a way to open jrnl by name

--- Packages/Jrnl/sublime-jrnl.py
+++ Packages/Jrnl/sublime-jrnl.py
@@ -85,13 +85,24 @@ class JrnlListCommand(sublime_plugin.ApplicationCommand):
         if index == -1:
             return
         file_path = self.file_list[index][1]
-        sublime.run_command("jrnl_open", {"file_path": file_path})
+        sublime.run_command("jrnl_open", {"jrnl": file_path})
 
 
 class JrnlOpenCommand(sublime_plugin.ApplicationCommand):
 
-    def run(self, file_path):
-        sublime.set_timeout(lambda: self.async_open(file_path), 0)
+    def run(self, jrnl, is_path=True):
+        # jrnl name was passed, and not it's path
+        if not is_path:
+
+            # TODO: Refactor this
+            config = load_and_fix_json(get_config_path())
+
+            for name, path in config['journals'].items():
+                # TODO: Handle else case, and throw error
+                if name == jrnl:
+                    jrnl = path
+
+        sublime.set_timeout(lambda: self.async_open(jrnl), 0)
 
     def async_open(self, file_path):
         view = (
